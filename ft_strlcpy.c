6dc2b077d2e525ea02c3b6a27fa2823b8b36c25b
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-05-14 13:16:45 -0300

    Update ft_strlcpy.c
    atualizando

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,34 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ memmove.c                                      :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/12 10:55:51 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/05/14 12:27:35 by fpeixoto         ###   ########.fr       */
+/*   Created: 2022/05/11 20:06:06 by Felipe Peix       #+#    #+#             */
+/*   Updated: 2022/05/14 13:15:07 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include "libc.h"
-
-void    *ft_memmove(void *dst, const void *src, size_t len)
+#include "libft.h"
+size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
 {
-    int i;
-    char *ptr;
+	unsigned int n;
+	int i;
 
-    i = 0;
-    ptr = dst;
+	i = 0;
+	n = dstsize;
 
-    while ( len > 0)
-    {
-        while (ptr[i] != '\0')
-        {
-            ptr[i++] = (*(char *)src++);
-            len--;
-        }
-    }
-    return (ptr);
-
+	while (n > 0)
+	{
+		dst[i] = src [i];
+		n--;
+		i++;
+	}
+	return (*(char *)dst);
 }
