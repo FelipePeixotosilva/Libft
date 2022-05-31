cc8c8fde04d43303d258b01817f904eb2634ca01
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-05-31 19:51:37 -0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,37 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 17:54:23 by Felipe Peix       #+#    #+#             */
+/*   Updated: 2022/05/14 13:07:07 by fpeixoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-void *ft_memchr(const void *s, int c, size_t n)
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {
-	unsigned char *ptr1;
-	unsigned char *ptr2;
-	size_t i;
-	size_t pos;
-
-	ptr1 = ((unsigned char *) s);
-	ptr2 = ((unsigned char *) ptr1);
-	i = 0;
-	pos = 0;
-	
-	if(ft_strlen((const char *)ptr1) == 0)
-	{
-		return "(null)";
-	}
-
-	while (n > 0)
-	{
-		if(ptr1[i] == c)	
-		{
-			while(ptr1[i] != '\0')
-			{
-				ptr2[pos] = ptr1[i];
-				i++;
-				pos++;
-			}	
-		}
-		i++;
-		n--;	
-}
-		ptr2[pos] = '\0';
-		return (ptr2);
+        int i;
+        int cont;
+        size_t s;
+		size_t dec = (size - (ft_strlen(dst)-1));	
+		cont = ft_strlen(dst);
+        s = ft_strlen(dst) + ft_strlen(src);
+        i = 0;
+        
+        if(size > 0)
+        {
+               
+                while (dec  > 0)
+                {
+                        dst [cont] = src [i];
+                        cont++;
+                        i++;
+                        dec--;
+						printf("%lu\n", (size - (ft_strlen(dst)-1)));
+                }
+                dst [cont] = '\0';
+                return s;
+        }
+        return (ft_strlen(src) + size) ;
 }
