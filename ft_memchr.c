8c4efebd3adaf11870f0981bcc7f0c064514a8ac
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-05-31 19:51:22 -0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,31 +3,35 @@
 
 void *ft_memchr(const void *s, int c, size_t n)
 {
-unsigned char *ptr1;
-unsigned char *ptr2;
-int i;
-int pos;
+	unsigned char *ptr1;
+	unsigned char *ptr2;
+	size_t i;
+	size_t pos;
 
-ptr1 = ((char *)s);
-i = 0;
-while (n > 0)
-{
-	if (ptr1[i] == c)
-	{	
-		pos = i;
-		i = 0;
-		while (ptr1[pos] != '\0')
-		{
-			ptr2[i] = ptr1[pos];
-			i++;
-			pos++;
-		}
-		ptr2[i++] = '\0';
-		return (ptr2);
+	ptr1 = ((unsigned char *) s);
+	ptr2 = ((unsigned char *) ptr1);
+	i = 0;
+	pos = 0;
+	
+	if(ft_strlen((const char *)ptr1) == 0)
+	{
+		return "(null)";
 	}
-	i++;
-	n--;
-}
 
-return NULL;
+	while (n > 0)
+	{
+		if(ptr1[i] == c)	
+		{
+			while(ptr1[i] != '\0')
+			{
+				ptr2[pos] = ptr1[i];
+				i++;
+				pos++;
+			}	
+		}
+		i++;
+		n--;	
+}
+		ptr2[pos] = '\0';
+		return (ptr2);
 }
