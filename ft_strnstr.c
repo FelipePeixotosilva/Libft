ab74516e8b5c2e87c6812f3dbbdf970e60fa851e
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-02 21:14:24 -0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,6 +1,33 @@
 #include "libft.h"
 
-char *ft_strnstr(const char *big, const char *little, size_t len)
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-return "c";
+	size_t i;
+	size_t j;
+	
+	i = 0;
+	j = 0;
+
+	while(haystack[i] != '\0')
+	{
+		if  (haystack[i] == needle[0])
+		{
+			while(haystack[i+j] == needle[j])
+			
+				j++;
+
+				if(needle[j] == '\0')
+				{
+					return ((char *)&haystack[i]);
+				}
+				else 
+				{
+					return NULL;
+				}	
+			
+		}
+		i++;
+	}
+	return NULL;
 }
