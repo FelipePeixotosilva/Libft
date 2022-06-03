79eb3236b93ca1af9e85c4a42ed717877f2d109a
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-03 07:00:51 -0300

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -2,27 +2,32 @@
 
 int ft_atoi(const char *nptr)
 {
-        size_t i;
-        int s;
-        int c = 1;
+	size_t i;
+	int s;
+	int c = 1;
 
-        i = 0;
-        s = 0;
+	i = 0;
+	s = 0;
 
+	
+	while (nptr[i] != '\0')
+	{
+		if(ft_isalpha(nptr[i]) == 1)
+		{
+			return s * c;
+		}
+		else if((nptr[i] != (" " || ft_isdigit(nptr[i])))
+			&& (nptr[i] == '-'))		
+		{
+			c = -1;
+		}
 
-        while (nptr[i] != '\0')
-        {
-                if(ft_isalpha(nptr[i]) == 1)
-                {
-                        return s * c;
-                }
-
-                else if (nptr[i] >= '0' && nptr[i] <= '9')
-                {
-                        s = (s * 10 + (nptr[i] - 48));
-                }
-                i++;
-        }
-        return s * c;
+		else if (ft_isdigit(nptr[i]) == 1)
+		{
+			s = (s * 10 + (nptr[i] - 48));
+		}	
+		i++;
+	}
+	return s * c;
 
 }
