8bfe22c65b7d216d9862ece160b86200269bc8f1
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-04 18:04:34 -0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,22 +2,16 @@
 
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char *ptr1;
-	unsigned const char *ptr2;
-	int i;
-
-	ptr1 = dest;
-	ptr2 = src;
+	size_t i;
+	
 	i = 0;	
 
-	ft_bzero(ptr1,n);
 	while(n > 0)
 	{
-		ptr1[i] = ptr2[i];
+		((char *)dest)[i] = ((char *)src)[i];
 		i++;
 		n--;
 	}
-	
-
-		return (ptr1);
+		
+		return (dest);
 }
