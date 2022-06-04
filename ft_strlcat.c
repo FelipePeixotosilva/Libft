be1a4d1a08f3b741efde8c396028e215651e4689
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-04 19:53:13 -0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,25 +17,24 @@ size_t  ft_strlcat(char *dst, const char *src, size_t size)
         int i;
         int cont;
         size_t s;
-
+		size_t dec = (size - (ft_strlen(dst)-1));	
+		cont = ft_strlen(dst);
         s = ft_strlen(dst) + ft_strlen(src);
-        i = 0;
-        cont = 0;
+        i = -1;
+        
         if(size > 0)
         {
-                while (ft_isprint(dst[cont]) == 1)
-                {
-                        cont++;
-                }
-                while (size > 0)
+               
+                while (dec  > 0)
                 {
                         dst [cont] = src [i];
                         cont++;
                         i++;
-                        size--;
+                        dec--;
+				
                 }
                 dst [cont] = '\0';
                 return s;
         }
-        return size;
+        return (ft_strlen(src) + size) ;
 }
