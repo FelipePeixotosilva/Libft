ef13ceb2d438699d19f656c558dcdabd05224825
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-04 18:03:55 -0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,41 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/12 17:54:23 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/05/14 13:07:07 by fpeixoto         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
+void *ft_memchr(const void *s, int c, size_t n)
 {
-        int i;
-        int cont;
-        size_t s;
-		size_t dec = (size - (ft_strlen(dst)-1));	
-		cont = ft_strlen(dst);
-        s = ft_strlen(dst) + ft_strlen(src);
-        i = 0;
-        
-        if(size > 0)
-        {
-               
-                while (dec  > 0)
-                {
-                        dst [cont] = src [i];
-                        cont++;
-                        i++;
-                        dec--;
-						printf("%lu\n", (size - (ft_strlen(dst)-1)));
-                }
-                dst [cont] = '\0';
-                return s;
-        }
-        return (ft_strlen(src) + size) ;
+	size_t i;
+	i = 0;
+
+	while (n > 0)
+	{
+		if(((char *)s)[i] == c)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+		n--;
+	}
+	return (NULL);
 }
