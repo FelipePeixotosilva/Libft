4e66c852f29639fe465cf31609e3d8862e2d3531
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-05 22:19:35 -0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,17 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/05 03:54:44 by Felipe Peix       #+#    #+#             */
+/*   Updated: 2022/06/05 04:08:11 by Felipe Peix      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	
-	i = 0;	
+	size_t	i;
 
-	while(n > 0)
+	i = 0;
+	while ((((char *)src)[i] != '\0') && (n > 0))
 	{
 		((char *)dest)[i] = ((char *)src)[i];
 		i++;
-		n--;
 	}
-		
-		return (dest);
+	((char *)dest)[i] = '\0';
+	return ((char *)dest);
 }
