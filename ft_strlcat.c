dc01569d7a2ed473c540bedff8a16810e1380326
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-05 22:25:22 -0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,38 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/12 17:54:23 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/05/14 13:07:07 by fpeixoto         ###   ########.fr       */
+/*   Created: 2022/06/05 18:49:27 by Felipe Peix       #+#    #+#             */
+/*   Updated: 2022/06/05 21:04:40 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-        int i;
-        int cont;
-        size_t s;
-		size_t dec = (size - (ft_strlen(dst)-1));	
-		cont = ft_strlen(dst);
-        s = ft_strlen(dst) + ft_strlen(src);
-        i = -1;
-        
-        if(size > 0)
-        {
-               
-                while (dec  > 0)
-                {
-                        dst [cont] = src [i];
-                        cont++;
-                        i++;
-                        dec--;
-				
-                }
-                dst [cont] = '\0';
-                return s;
-        }
-        return (ft_strlen(src) + size) ;
+	size_t i;
+	size_t j;
+	
+	i = ft_strlen(dst);
+	j = 0;
+	if(size > 0)
+	{
+		while ((src[j] != '\0') && (i < (size - (ft_strlen(dst)-1))))
+		{
+			dst[i] = src[j];
+			i++;
+			j++;
+		}
+		return (ft_strlen(dst) + ft_strlen(src));
+	}
+	return (size + ft_strlen(src));
 }
