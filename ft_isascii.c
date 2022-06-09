a2f9b1b8a388a4d66891ba198d2a2abe934ead6a
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-09 07:08:29 -0300

    Update ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,13 +6,13 @@
 /*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 02:12:53 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/05 02:14:55 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/09 00:08:57 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isascii(int c)
 {
-	if ((c > 0) && (c < 177))
+	if ((c >= 0) && (c <= 127))
 	{
 		return (1);
 	}
