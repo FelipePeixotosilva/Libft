07e419318cdaec3bea96e119543fe53bf5765d9f
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-11 18:56:27 -0300

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 11:42:39 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/05 12:43:48 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/11 17:53:31 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-
-	i = 0;
-	if (size > 0)
+	if (ft_strlen(src) + 1 < size)
 	{
-		while (dst[i] != '\0' && size > 0)
-		{
-			dst[i] = src[i];
-			i++;
-			size--;
-		}
+		ft_memmove(dst, src, ft_strlen(src) + 1);
+		dst[size - 1] = '\0';
 	}
-	return (i);
+	else if (size != 0)
+	{
+		ft_memmove(dst, src, size - 1);
+		dst[size - 1] = '\0';
+	}	
+	return (ft_strlen(src));
 }
