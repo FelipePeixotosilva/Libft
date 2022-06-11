2ca3cf9512da68fc0ea37d07f59f80bc46e3b129
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-11 16:00:27 -0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,28 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 20:11:18 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/06/07 20:48:21 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/06/11 15:51:29 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-    void *ptr;
+	void	*ptr;
 
-    if (count == 0 || size == 0)
-    {
-        return NULL;
-    }
-
-    ptr = (void *)malloc((size * count));
-    ft_bzero(ptr,(size * count));
-
-    return (ptr);
+	if (count == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (void *)malloc((size * count));
+	ft_bzero(ptr, (size * count));
+	return (ptr);
 }
 
 /*#include <stdio.h>
