09a469be047ff19025f71d0fd750af4af4edc9b4
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-11 15:47:50 -0300

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 18:13:04 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/05 18:42:28 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/11 15:46:39 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while ((n > 0) && ((unsigned char *)src)[i] != '\0')
+	if (dest > src)
 	{
-		(((unsigned char *)dest)[i]) = ((unsigned char *)src)[i];
-		i++;
-		n--;
+		while (n > 0)
+		{
+			n--;
+			((char *)dest)[n] = ((char *)src)[n];
+		}
 	}
-	return (((unsigned char *)dest));
+	else
+	{
+		ft_memcpy(dest, src, n);
+	}
+	return (dest);
 }
