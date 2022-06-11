ef7e737f063bdce01f1a2fa557b4e12543a8fc93
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-11 16:25:16 -0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 17:39:50 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/09 20:57:54 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/06/11 16:23:54 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void *ft_memchr(const void *s, int c, size_t n)
 {
-	const char *ptr;
+	unsigned const char *ptr;
 	size_t i;
 
 	i = 0;
 	ptr = s;
 	while (n > 0)
 	{
-		if (ptr[i] == c)
+		if (((char *)s)[i] == c)
 		{
 			return ((void *) &ptr[i]);
 		}
