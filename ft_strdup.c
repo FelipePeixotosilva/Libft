212f91324952b2223efcaa361b4e6f27ebb5c278
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-11 17:01:52 -0300

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,39 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 19:36:11 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/06/07 20:09:30 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/06/11 16:56:58 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-    char    *ptr;
-    size_t  i;
+	char	*ptr;
+	size_t	i;
 
-    i = 0;
-    if(s1 == NULL)
-    return (NULL);
-
-    ptr = (char *)malloc(sizeof(s1));
-    while (s1[i] != '\0')
-    {
-        ptr[i] = s1[i];
-        i++;
-    }
-    return (ptr);
+	i = 0;
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	ptr = (char *)malloc(ft_strlen(s1) + 1);
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
-
-/*#include <stdio.h>
-int main()
-{
-    char str1[]= "\0";
-    char *str2;
-    str2 = ft_strdup(str1);
-    printf("%s\n", str2);
-}
-*/
