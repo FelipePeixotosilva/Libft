e35de09f64b0cd224d26f52483faa47573c1705f
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-11 22:25:55 -0300

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 17:13:13 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/11 22:18:36 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/11 22:24:52 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (n > 0)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		i++;
 		n--;
-		
 	}
 	return (0);
 }
