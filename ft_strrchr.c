106e32d2e39848c7dcfc3718814842e43d0f4c5d
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-15 10:03:35 -0300

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 04:39:49 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/11 16:35:00 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/15 10:02:37 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
-	i = ft_strlen(s) + 1;
-	while (i > 0)
+	i = ft_strlen(s)+1;
+	
+	while ((i > 0))
 	{
 		i--;
-		if (s[i] == c)
+		if (s[i] == (char) c)
 		{
 			return ((char *)&s[i]);
 		}	
 	}
+	
 	return (NULL);
 }
