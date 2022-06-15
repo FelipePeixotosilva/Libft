69c06fe6a2fb14231967448cc5442d255d4eb32b
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-15 09:20:40 -0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,46 +6,39 @@
 /*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 05:07:59 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/15 01:35:10 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/15 09:19:32 by Felipe Peix      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t cont;
 	size_t i;
+	size_t j;
 	
 	
-	cont = 0;
+	j = 0;
 	i = 0;
-	if ((len  >= ft_strlen(big)))
-		len = ft_strlen(big)+1;
-	if(len != 0)
-	{	
-	while ((i <= len))
+	if(*little == 0)
 	{
-		if(cont == ft_strlen(little))
+		return (char *)big;
+	}
+	if ((len == 0) || (*big == 0))
+	{
+		return NULL;
+	}
+	while (i < len && big[i])
+	{
+		j = 0;
+		while ((i + j < len) && (big[i + j] == little[j]) && little[j])
 		{
-			 i = i - cont;
-			return ((char *)&big[i]);
+			j++;
+		if (little[j] == '\0')
+		{
+			return ((char *)big + i);
 		}
-		else if (big[i] == little[cont])
-		{
-			cont++;
-		}
-		else if (cont > 0)
-		{
-			i = i - (cont - 1);
-			cont = 0;
-			if(big[i] == little[cont])
-			{
-				cont ++;
-			}
 		}
 		i++;
 	}
-	}
-
 	return (NULL);
 }
