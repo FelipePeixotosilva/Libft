d8222c5798856d47a46d523c3fafce2f007664fb
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-15 21:34:54 -0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,35 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/08 18:10:11 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/06/08 19:54:23 by fpeixoto         ###   ########.fr       */
+/*   Created: 2022/06/15 19:13:11 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/06/15 20:45:47 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char    *ft_strjoin(char const *s1, char const *s2)
+void ft_putnbr_fd(int n, int fd)
 {
-    char *ptr;
-    size_t s;
-    
-    s = (ft_strlen(s1) + ft_strlen(s2))+1;
-    ptr = (char *)malloc(s);
-    ft_strlcat(ptr,s1,sizeof(s1)+1);
-    ft_strlcat(ptr,s2,sizeof(s2)+1);
-    
-    return(ptr);
+    if(n == -2147483648)
+    {
+        ft_putstr_fd("-2147483648",fd);
+        return;
+    }
+    if(n == 2147483647)
+    {
+        ft_putstr_fd("2147483647",fd);
+        return;
+    }
+    if(n < 0)
+    {
+        ft_putchar_fd('-',fd);
+        n = n * -1;
+    }
+    if(n >= 0 && n <= 9)
+    {
+        ft_putchar_fd(n + '0',fd);
+    }
+    else
+    {
+        ft_putnbr_fd(n/10,fd);
+        ft_putnbr_fd(n%10,fd);
+    }    
 }
 
-/*#include <stdio.h>
-int main()
-{
-    char *test;
-    test = ft_strjoin("esta","certo");
-    printf("%s\n", test);
-}
-*/
