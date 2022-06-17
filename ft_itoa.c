dc18d2ddf08f1d68fc975361736a6a0b6c09abc4
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-17 02:30:26 -0300

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,78 +6,65 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 16:15:45 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/06/16 19:57:54 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/06/17 01:55:55 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int cvrt(int n)
+static	int	cvrt(int n)
 {
-   return n = n * -1;
-}
-static int qnbr(int n)
-{
-    int i;
-    i = 0;
-    if ( n < 0)
-        {
-            i++;
-            n = cvrt(n);
-        }
-    while ( n > 0)
-    {
-        n = n / 10;
-        i++;
-    }
-    return (i);
+	if (n < 0)
+		return (n = n * -1);
+	return (n);
 }
 
-char *ft_itoa(int n)
+static int	qnbr(int n)
 {
-    size_t i;
-    char *ptr;
-    
-    if(n == -2147483648)
-    {
+	int	i;
 
-        return ft_strdup("-2147483648");
-    }
-    if(n == 2147483647)
-    {
-         return ft_strdup("2147483647");
-    }
-    i = 0;
-    ptr = (char *)malloc((sizeof(char) * qnbr(n))+1);
-    if(ptr == 0)
-    {
+	i = 0;
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		i++;
+		n = cvrt(n);
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
 
-        return (NULL);   
-    }
-    if(n < 0)
-    {
-        ptr[i] = '-';
-        i++;
-        n = cvrt(n);
-    }
-    while (i < (size_t)qnbr(n))
-    {
-        if(n >= 0 && n <= 9 )
-        {
-            ptr[i] = n + '0';
-            i++;
-        }
-        else
-        {
-            n = n / 10;
-            ptr[i] = n + '0';
-            i++;
-            n = n % 10;
-            ptr[i] = n + '0'; 
-            i++; 
-        }
-    
-    }
-    ptr[i] = '\0';
-    return (ptr);
+char	*ft_itoa(int n)
+{
+	size_t	i;
+	char	*ptr;
+
+	if (n == -2147483648)
+	{
+		return (ft_strdup("-2147483648"));
+	}
+	i = qnbr(n);
+	ptr = (char *)malloc((sizeof(char) * qnbr(n)) + 1);
+	if (n == 0)
+		ptr[0] = '0';
+	ptr[i] = '\0';
+	if (n < 0)
+	{
+		ptr[0] = '-';
+		n = cvrt(n);
+	}
+	while (n > 0)
+	{
+			i--;
+			ptr[i] = i n % 10 + '0';
+			n = n / 10;
+	}
+	return (ptr);
 }
