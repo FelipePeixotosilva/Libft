b3706daf26df53381a6903a7c1c02f62457a37b0
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-21 19:36:26 -0300

    Create ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 18:32:22 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/06/21 19:34:42 by fpeixoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+   
+    size_t start;
+    size_t end;
+    char *ptr;
+    
+    if(s1 == NULL || set == NULL )
+        return NULL;
+    start = 0;
+    while (s1[start] && ft_strchr(set, s1[start]))
+        start++;
+    end = ft_strlen(s1);
+    while (end > start && ft_strrchr(set, s1[end - 1]))
+        end--;
+    ptr = ft_substr(s1,start,end);
+    if(ptr == 0)
+        return NULL;
+    
+    ptr[end++] = '\0'; 
+    return ptr;
+}
+
+/*#include <stdio.h>
+int main()
+{
+    char *ptr;
+    ptr = ft_strtrim("felipepe", "fi");
+    printf("%s\n", ptr);
+    
+}*/
