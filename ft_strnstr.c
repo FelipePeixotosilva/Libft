3610b1432c5566fc6430c64e81584d28ca94aa04
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-21 06:07:51 -0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,40 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 05:07:59 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/15 09:23:04 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/21 06:07:18 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t i;
-	size_t j;
-	
-	
+	size_t	i;
+	size_t	j;
+
 	j = 0;
 	i = 0;
-	if(*little == 0)
-	{
-		return (char *)&big[i];
-	}
+	if (*little == 0)
+		return ((char *)&big[i]);
 	if ((len == 0) || (*big == 0))
-	{
-		return NULL;
-	}
+		return (NULL);
 	while (i < len && big[i])
 	{
 		j = 0;
 		while ((i + j < len) && (big[i + j] == little[j]) && little[j])
 		{
 			j++;
-		if (little[j] == '\0')
-		{
-			return ((char *)&big[i]);
-		}
+			if (little[j] == '\0')
+				return ((char *)&big[i]);
 		}
 		i++;
 	}
