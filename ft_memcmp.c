2409ff991fe8320dada61fcc0b3be76d4937bd0c
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-21 05:57:42 -0300

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:45:12 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/09 18:35:03 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/06/21 05:57:09 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
-	unsigned const char *ptr1;
-	unsigned const char *ptr2;
+	size_t				i;
+	unsigned const char	*ptr1;
+	unsigned const char	*ptr2;
 
 	ptr1 = s1;
 	ptr2 = s2;
