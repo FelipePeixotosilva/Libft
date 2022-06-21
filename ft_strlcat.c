80e610b9a5feab38ef180d6b7ec06696afe89f5a
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-21 07:03:19 -0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 18:49:27 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/15 19:00:50 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/06/21 07:02:57 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,22 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t dst_len;
-	size_t j;
-	size_t limit;
-	
+	size_t	dst_len;
+	size_t	j;
+	size_t	limit;
+
 	dst_len = ft_strlen(dst);
-	if(size <= dst_len)
+	if (size <= dst_len)
 	{
-		return size + ft_strlen(src);
+		return (size + ft_strlen(src));
 	}
-	
 	limit = size - dst_len - 1;
 	j = 0;
-	
 	while (src[j] && j < limit)
 	{
 		dst[dst_len + j] = src[j];
 		j++;
 	}
 	dst[dst_len + j] = '\0';
-	return dst_len + ft_strlen(src);
+	return (dst_len + ft_strlen(src));
 }
