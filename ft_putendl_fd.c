398469e14c1863cb4704f898eb55c53eaa002c7d
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-06-21 06:26:48 -0300

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Felipe Peixoto <felipe.peixoto@msn.com>    +#+  +:+       +#+        */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 14:22:08 by Felipe Peix       #+#    #+#             */
-/*   Updated: 2022/06/16 11:13:45 by Felipe Peix      ###   ########.fr       */
+/*   Updated: 2022/06/21 06:26:21 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-	if(s == NULL)
+	if (s == NULL)
 	{
 		return ;
 	}
-	write(fd, s, ft_strlen(s));
-	write(fd,"\n",1);
+	write (fd, s, ft_strlen(s));
+	write (fd, "\n", 1);
 }
